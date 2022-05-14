d206ccec3874c5ffdd0af42230f65269bb7896ef
Tidbits JS <tidbitsjs@gmail.com>
2022-05-14 23:45:11 +0530

    plantql setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Connectio to database failed");
+    console.error(error);
+  }
+};
+
+connectToMongo();
+
+app.listen(4040, () => {
+  console.info("GraphQL server running on port 4040");
+});
